1508ddce6070dd4c9599da66dbdca7de7668775e
Felipe Menanteau <felipe@illinois.edu>
2018-05-17 15:02:55 -0500

    Intermediate changes

--- python/HeaderService/__init__.py
+++ python/HeaderService/__init__.py
@@ -4,11 +4,11 @@ version = __version__
 
 from . import hutils
 from . import camera_coords
-from .hutils import HDRTEMPL_SciCamera
+#from .hutils import HDRTEMPL_SciCamera
 from .hutils import HDRTEMPL_TestCamera
 from .hutils import HDRTEMPL_ATSCam
 from .camera_coords import CCDGeom
-from . import SAL_tools
+#from . import SAL_tools
 #from . import SAL_tools_send
 from . import states
 
