19e96d8d493aa5a7fd08b0f4170cad7fb67d9ed6
Felipe Menanteau <felipe@illinois.edu>
2018-07-13 17:36:05 -0500

    Minor update to bash script that starts web services

--- bin/start_www.sh
+++ bin/start_www.sh
@@ -6,7 +6,7 @@ PORT_NUMBER=$2
 # Make sure it not running
 pid=`ps -ax | grep SimpleHTTPServer  | grep -v grep | awk '{print $1}'`
 
-if [ $pid > 0 ]; then
+if [[ $pid > 0 ]]; then
     echo "SimpleHTTPServer already running on $pid... Bye"
 else
     echo "Starting SimpleHTTPServer"
