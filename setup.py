cb9944db91f75029c098fb4351a7d946cddb178f
Felipe Menanteau <felipe@illinois.edu>
2019-05-31 16:50:55 -0500

    Rolled up version to 0.9.5 and SAL 3.10.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ for edir in etc_dirs:
 
 # The main call
 setup(name='HeaderService',
-      version ='0.9.5',
+      version ='0.9.6',
       license = "GPL",
       description = "LSST Meta-data aggregator for FITS header service",
       author = "LSST, Felipe Menanteau",
