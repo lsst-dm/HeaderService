fbdb0aba81d49b2468ad2cc94e95b7c9a7324594
Felipe Menanteau <felipe@illinois.edu>
2021-07-08 15:52:24 -0500

    Adding EMUIMAGE to HeaderService

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ for edir in etc_dirs:
 
 # The main call
 setup(name='HeaderService',
-      version='2.9.1',
+      version='2.9.2',
       license="GPL",
       description="LSST Meta-data aggregator for FITS header service",
       author="LSST, Felipe Menanteau",
