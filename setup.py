617e99ea3ae4d52426c15aca74084974a4d14ad4
Felipe Menanteau <felipe@illinois.edu>
2022-02-10 13:07:37 -0300

    Adding CURINDEX and MAXINDEX to headers
    Adding CURINDEX and MAXINDEX to HeaderService

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ for edir in etc_dirs:
 
 # The main call
 setup(name='HeaderService',
-      version='2.9.4',
+      version='2.9.5',
       license="GPL",
       description="LSST Meta-data aggregator for FITS header service",
       author="LSST, Felipe Menanteau",
