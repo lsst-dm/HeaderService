d48ce7143e95dbbaf072cb1f2a64124396ec79d8
Felipe Menanteau <felipe@illinois.edu>
2023-02-21 13:57:41 -0300

    Update version to 3.1.8

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ for edir in etc_dirs:
 
 # The main call
 setup(name='HeaderService',
-      version='3.1.7',
+      version='3.1.8',
       license="GPL",
       description="LSST Meta-data aggregator for FITS header service",
       author="LSST, Felipe Menanteau",
