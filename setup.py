a4d1a226e71d74660bac2effde0d7f85a10ec411
Felipe Menanteau <felipe@illinois.edu>
2023-03-20 17:58:21 -0500

    Updates to ROTPA in GenericCameras

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ for edir in etc_dirs:
 
 # The main call
 setup(name='HeaderService',
-      version='3.1.9',
+      version='3.1.10',
       license="GPL",
       description="LSST Meta-data aggregator for FITS header service",
       author="LSST, Felipe Menanteau",
