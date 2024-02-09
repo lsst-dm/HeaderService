e80c8bb588c5686ab386350fbf0c78c72e7b73f7
Felipe Menanteau <felipe@illinois.edu>
2024-02-08 21:14:36 -0300

    Hot fix to setup.py to install json files

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ bin_files = glob.glob("bin/*")
 # Build the structure for etc folder
 etc_dirs = ['etc/LATISS', 'etc/ComCam', 'etc/GenericCamera-1',
             'etc/GenericCamera-101', 'etc/GenericCamera-102', 'etc/GenericCamera-103',
-            'etc/TestCamera/E2V', 'etc/TestCamera/ITL', 'etc/conf']
+            'etc/TestCamera/E2V', 'etc/TestCamera/ITL', 'etc/conf',
+            'etc/playback/lib/ComCam']
 data_files = [("", ["setpath.sh"])]
 for edir in etc_dirs:
     data_files.append((edir, glob.glob("{}/*".format(edir))))
@@ -15,7 +16,7 @@ for edir in etc_dirs:
 
 # The main call
 setup(name='HeaderService',
-      version='3.3.2',
+      version='3.3.3',
       license="GPL",
       description="LSST Meta-data aggregator for FITS header service",
       author="LSST, Felipe Menanteau",
