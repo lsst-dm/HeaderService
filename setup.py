78f93f76b309ee718a5037b356ceac6439e7eac7
Felipe Menanteau <felipe@illinois.edu>
2024-03-21 16:21:00 -0500

    Update json files for playback

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ for edir in etc_dirs:
 
 # The main call
 setup(name='HeaderService',
-      version='3.3.8',
+      version='3.3.10',
       license="GPL",
       description="LSST Meta-data aggregator for FITS header service",
       author="LSST, Felipe Menanteau",
