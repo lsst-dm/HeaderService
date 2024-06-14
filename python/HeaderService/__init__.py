a757e780ae1891487beb7345ca96bd3b5d782f27
Felipe Menanteau <felipe@illinois.edu>
2024-06-14 12:08:42 -0500

    New json files for OR4

--- python/HeaderService/__init__.py
+++ python/HeaderService/__init__.py
@@ -20,7 +20,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 __author__ = "LSST/Felipe Menanteau"
-__version__ = "3.4.4"
+__version__ = "3.4.5"
 version = __version__
 
 from . import hutils
