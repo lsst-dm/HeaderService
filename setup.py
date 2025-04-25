225d65a1ff9495b2da28424f7a271a92979272e1
Felipe Menanteau <felipe@illinois.edu>
2025-04-25 15:54:48 -0500

    Preparing TAG 3.5.7

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ for edir in etc_dirs:
 
 # The main call
 setup(name='HeaderService',
-      version='3.5.6',
+      version='3.5.7',
       license="GPL",
       description="LSST Meta-data aggregator for FITS header service",
       author="LSST, Felipe Menanteau",
